4a065a76fcb7ec74eb53cf0b3106e1535aa9d86c
TrongPhucNguyen <trongnguyenphuc22082003@gmail.com>
2024-09-17 10:01:03 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './Pages/Common/Login/Login';
+import HomePage from './Pages/Users/HomePage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   )
