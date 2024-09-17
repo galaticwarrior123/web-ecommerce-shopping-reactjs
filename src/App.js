aaf3b1ad65aa6877b75c978b546bdb7539d6e506
PhuongNgan2304 <phuongngan.nguyen2304@gmail.com>
2024-09-17 09:15:12 +0700

    đang updtae quên mk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
+import ForgotPassword from './Pages/Common/ForgotPassword/ForgotPassword'
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={Home} />
+      {/* <Route path="/" component={Home} /> */}
+      <Route path="/forgot-password" element={<ForgotPassword/>}/>
     </BrowserRouter>
   )
 }
