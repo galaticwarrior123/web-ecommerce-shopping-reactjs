a319bdc07989d087016aac3c57a6f3f8ba7d88b0
PhuongNgan2304 <phuongngan.nguyen2304@gmail.com>
2024-09-19 22:46:15 +0700

    Adjust file App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './Pages/Common/Login/Login';
-<<<<<<< HEAD
-import HomePage from './Pages/Users/HomePage';
-import ForgotPassword from './Pages/Common/ForgotPassword/ForgotPassword'
-=======
 import ForgotPassword from './Pages/Common/ForgotPassword/ForgotPassword'
 import HomePage from './Pages/Users/HomePage';
->>>>>>> main
 
 import Register from './Pages/Common/Register/Register';
 import Verify from './Pages/Common/Register/Verify';
 function App() {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-      {/* <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes> */}
-=======
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
@@ -25,7 +15,6 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
->>>>>>> main
     </BrowserRouter>
   )
 }
